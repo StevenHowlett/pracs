10c6f59c1b0d342a3761e6b3ff2f4776f6d8c4a6
jc484989 <steven.howlett@my.jcu.edu.au>
2018-09-06 13:41:49 +1000

    increase  readabilty

--- prac5/word_occurrences.py
+++ prac5/word_occurrences.py
@@ -2,12 +2,15 @@ word_to_count = {}
 user_text = input('text')
 words = user_text.split()
 longest_word_length = 0
+
 for word in words:
     if word not in word_to_count:
         word_to_count[word] = 1
     else:
         word_to_count[word] += 1
+
     if len(word) > longest_word_length:
         longest_word_length = len(word)
-for word,count in sorted(word_to_count.items()):
-    print("{:<{}} : {}".format(word,longest_word_length,count))
+
+for word, count in sorted(word_to_count.items()):
+    print("{:{}} : {}".format(word, longest_word_length, count))
